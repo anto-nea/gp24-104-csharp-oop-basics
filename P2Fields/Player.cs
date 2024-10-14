101f1c444fad7192a0f4c5af16a542698a8aa70e
anto-nea <antonia.tomus@gmail.com>
2024-10-14 11:17:01 +0200

    fields

---
+++ P2Fields/Player.cs
@@ -0,0 +1,18 @@
+namespace P2Fields;
+
+public class Player
+{
+    public string Name;
+    public int Health;
+    public float Mana;
+
+    public string PrintClassVariables()
+    {
+        return this.Name + * * + this.Health + * * + this.Mana;
+    }
+
+void SomethingPrivate()
+{
+    Console.WriteLine("Something private");
+}
+}
---
+++ P2Fields/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Player
+{
+    public int Gold, Stone, Wood, Level, Experience;
+    public bool VipActive, PvpEnabled;
+    public string DisplayName, Email;
+}
+
+class Program
+{
+    static void Main()
+    {
+        var player = new Player
+        {
+            Gold = 200,
+            Stone = 110,
+            Wood = 53,
+            Level = 12,
+            Experience = 123,
+            VipActive = true,
+            PvpEnabled = false,
+            DisplayName = "Marc",
+            Email = "marc@zaku.de"
+        };
+
+        Console.WriteLine($"Name: {player.DisplayName}, Email: {player.Email}");
+        Console.WriteLine($"Gold: {player.Gold}, Stone: {player.Stone}, Wood: {player.Wood}");
+        Console.WriteLine($"Level: {player.Level}, Experience: {player.Experience}");
+        Console.WriteLine($"VIP Active: {player.VipActive}, PVP Enabled: {player.PvpEnabled}");
+    }
+}
