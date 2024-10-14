21d6b302e30c080e6f9463064983132e5620f2f8
anto-nea <antonia.tomus@gmail.com>
2024-10-14 10:27:44 +0200

    add class and array

---
+++ P1People/Program.cs
@@ -0,0 +1,15 @@
+﻿class Person { }
+
+class Program
+{
+    static void Main()
+    {
+        var p1 = new Person();
+        var p2 = new Person();
+        var p3 = new Person();
+
+        Console.WriteLine(p1);
+        Console.WriteLine(p2);
+        Console.WriteLine(p3);
+    }
+}
