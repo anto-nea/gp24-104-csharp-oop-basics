726b3735ecb65908b73c2968981dd6e731b3d474
anto-nea <antonia.tomus@gmail.com>
2024-10-24 15:44:06 +0200

    papa don't preach

---
+++ P5_1Father/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace P5_1Father
+{
+ public class DarthVader
+    {
+    public void UseForce() => Console.WriteLine("Darth Vader is using the Force");
+    }
+
+ public class Luke : DarthVader
+    {
+    public void UseLightSaber() => Console.WriteLine("Luke is using his lightsaber");
+    }
+
+ public static class Program
+    {
+    public static void Main() 
+       {
+        var luke = new Luke();
+        luke.UseForce();
+        luke.UseLightSaber();
+        }
+    }
+}
